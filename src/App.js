f797d8ce02fc1b723bb8120e344bf3175fffa611
Kyle Thomas <mrkylethomas76@gmail.com>
2019-11-18 21:40:55 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+import Game from "../game/Game";
+
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: './assets/images/raccoon.png'
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: './assets/images/squirrel.png'
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: './assets/images/bunny.png'
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: './assets/images/hound.png'
+  }
+
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
+    </div>
+  )
+}
+
+  export default App;
